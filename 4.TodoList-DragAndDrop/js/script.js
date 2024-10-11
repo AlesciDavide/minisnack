cccef6a00d7a91c94e3c580d74614f8448a24b05
AlesciDavide <davi.alesci@gmail.com>
2024-10-11 11:29:06 +0200

    catturati gli eventi di drag: drastart, dragend, dragover, dragenter, dragleave, drop

--- 4.TodoList-DragAndDrop/js/script.js
+++ 4.TodoList-DragAndDrop/js/script.js
@@ -1,6 +1,7 @@
 /* selezioni gli elementi */
-const colonnaEl = document.querySelectorAll(".colonna");
+const colonneEl = document.querySelectorAll(".colonna");
 const taskEl = document.querySelectorAll(".task");
+let dragEl = null;
 
 /* foreach per prendere tutti gli elementi del querySelectorAll */
 taskEl.forEach(task =>{
@@ -8,16 +9,46 @@ taskEl.forEach(task =>{
     
     task.addEventListener("dragend", dragEnd);
 });
+colonneEl.forEach(colonne => {
+    colonne.addEventListener("dragover", dragOver);
+    colonne.addEventListener("dragenter", dragEnter);
+    colonne.addEventListener("dragleave", dragLeave);
+    colonne.addEventListener("drop", dragDrop);
+});
 
 
 
 
-/* funzioni */
+/* funzioni task */
 
 function dragStart(){
     console.log("dragStart");
+    setTimeout(() => {
+        this.style.display = "none"
+    }, 0);
+    dragEl = this;
 };
 
 function dragEnd(){
     console.log("dragEnd");
+};
+
+/* funzioni colonne */
+
+function dragOver(event){
+    event.preventDefault();
+    console.log("dragOver");
+};
+
+function dragEnter(){
+    console.log("dragEnter");
+};
+
+function dragLeave(){
+    console.log("dragLeave");
+};
+
+function dragDrop(){
+    console.log("dragDrop");
+    this.append(dragEl);
 };
