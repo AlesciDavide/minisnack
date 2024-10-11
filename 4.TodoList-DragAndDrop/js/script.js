09404c23e5a78718004dc58895b49e0ac693a9e5
AlesciDavide <davi.alesci@gmail.com>
2024-10-11 12:40:01 +0200

    utilizzo array per prendere le task e salvarle nelle colonne selezionate(possibilità di aggiungere una task tramite form )

--- 4.TodoList-DragAndDrop/js/script.js
+++ 4.TodoList-DragAndDrop/js/script.js
@@ -2,13 +2,56 @@
 const colonneEl = document.querySelectorAll(".colonna");
 const taskEl = document.querySelectorAll(".task");
 let dragEl = null;
+let dragData = null;
+
+const dataList = [
+    {
+        id: 0,
+        nome: "In coda",
+        tasks: [
+            {
+                id: 0,
+                testo: "Abilita il drag and drop",
+            },
+            {
+                id: 1,
+                testo: "Controlla se funziona bene",
+            },
+            {
+                id: 2,
+                testo: "Salvale in cache (successivamente salvarle in db)",
+            },
+        ]
+    },
+    {
+        id: 1,
+        nome: "Aperto",
+        tasks: [ {
+            id: 0,
+            testo: "prova id",
+        },]
+    },
+    {
+        id: 2,
+        nome: "In revisione",
+        tasks: []
+    },
+    {
+        id: 3,
+        nome: "chiuso",
+        tasks: []
+    },
+]
+
+
+
+/* genero le task partendo da dataList per poi poterlo sostituire con una chiamata api per il db per permettere di salvare i dati in modo permanente*/
+
+
+taskGenerate();
 
 /* foreach per prendere tutti gli elementi del querySelectorAll */
-taskEl.forEach(task =>{
-    task.addEventListener("dragstart", dragStart);
-    
-    task.addEventListener("dragend", dragEnd);
-});
+
 colonneEl.forEach(colonne => {
     colonne.addEventListener("dragover", dragOver);
     colonne.addEventListener("dragenter", dragEnter);
@@ -27,12 +70,24 @@ function dragStart(){
         this.style.display = "none"
     }, 0);
     dragEl = this;
+    const indexColonna = dataList.findIndex(colonna =>{
+        return colonna.id == this.parentElement.getAttribute("data-colonna");
+    })
+    const indexTask = dataList[indexColonna].tasks.findIndex(task =>{
+        return task.id == this.getAttribute("data-task");
+    })
+
+    dragData = dataList[indexColonna].tasks.splice(indexTask, 1);
 };
 
 function dragEnd(){
     console.log("dragEnd");
     dragEl.style.display = "block"
     dragEl = null;
+
+    dataList[this.parentElement.getAttribute("data-colonna")].tasks.push(dragData);
+    console.log(dataList, dragData);
+    
 };
 
 /* funzioni colonne */
@@ -55,5 +110,27 @@ function dragDrop(){
     this.append(dragEl);
 };
 
+/* funzioni per generare le task dall'array dataList */
+function taskGenerate(){
+    dataList.forEach(colonna =>{
+        const colonnaSelezionata = document.querySelector(`[data-colonna= '${colonna.id}']`)
+        colonna.tasks.forEach(task =>{
+            const elemento = document.createElement("div");
+            elemento.classList.add("task");
+            elemento.setAttribute("draggable", true);
+            elemento.setAttribute("data-task", task.id);
+            console.log(task.testo);
+            
+            const text = document.createTextNode(task.testo);
+            
+            elemento.appendChild(text);
+            colonnaSelezionata.appendChild(elemento);
 
-/* funzione per il cursore */
+            elemento.addEventListener("dragstart", dragStart);
+    
+            elemento.addEventListener("dragend", dragEnd);
+        });
+    });
+};
+
+
